ec5e74d4704edf9875046bc7af712f173dad2478
overcam <overcam1@gmail.com>
2016-06-24 06:40:59 +0000

    Timetable Gem Try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project1::Application.config.secret_key_base = '4be4abe7d8dba8e164319be3d394da2b72354a611334507ff9958fe2b6aba84e941b4c96c27144c3710ba8ea0654d41b5a4bc143d184f9dd1b24c8be4d9bd3aa'
